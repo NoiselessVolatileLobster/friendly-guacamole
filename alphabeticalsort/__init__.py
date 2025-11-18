f696906881a197bdbb62eac3982a181b8fe70137
NoiselessVolatileLobster <92268368+NoiselessVolatileLobster@users.noreply.github.com>
2025-11-18 16:41:26 -0600

    init

--- alphabeticalsort/__init__.py
+++ alphabeticalsort/__init__.py
@@ -1,5 +1,3 @@
 from .alphabeticalsort import setup
 
-# This line is essential for Red-DiscordBot to find and load your cog.
-# It tells Python to import the setup function from the same directory's 
-# 'alphabeticalsort.py' file.
+__red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
