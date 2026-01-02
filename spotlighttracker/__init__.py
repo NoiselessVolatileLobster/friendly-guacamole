50d2f44fea1ed4fa168b5e8a7b8a2cdf7823e96e
NoiselessVolatileLobster <92268368+NoiselessVolatileLobster@users.noreply.github.com>
2026-01-02 08:33:11 -0600

    typo in file name

--- spotlighttracker/__init__.py
+++ spotlighttracker/__init__.py
@@ -1,4 +1,4 @@
-from .spotlight import SpotlightTracker
+from .spotlighttracker import SpotlightTracker
 
 __red_end_user_data_statement__ = "This cog stores user IDs to track voice participation statistics during D&D sessions."
 
